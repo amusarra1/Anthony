0ccec1804ff682b8cce9a0d3e21c0554c5bc7a97
amusarra1 <56942183+amusarra1@users.noreply.github.com>
2019-10-23 19:42:06 -0400

    Add files via upload
    1st week

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Week1AnthonyMusarra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //This program adds 2 numbers.
+
+            string str1, str2;
+
+            str1 = "hello world";
+
+            str2 = "Lebron James";
+
+
+            Console.WriteLine(str1 + " from " + str2);
+
+
+
+
+
+
+
+
+
+
+            int num1, num2, num3;
+
+            num1 = 27;
+            num2 = 55;
+
+            num3 = num1 * num2;
+
+
+            Console.WriteLine("The Product of " +  " num1 "  + " and "  + num2 + " is "  + num3);
+        }
+    }
+}
