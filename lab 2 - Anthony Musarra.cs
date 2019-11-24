d64c46cc56a09dacf602144d1741301cbb489abb
amusarra1 <56942183+amusarra1@users.noreply.github.com>
2019-11-24 18:42:17 -0500

    Add files via upload

---
+++ lab 2 - Anthony Musarra.cs
@@ -0,0 +1,56 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+
+        Int(5)
+        Int(6)
+        Int(11)
+
+
+
+
+        Console.WriteLine("5 + 6")
+
+        Console.WriteLine("sum Is 11")
+
+
+        Console.WriteLine("Hello World from Anthony Musarra!")
+
+
+
+
+
+
+        Console.WriteLine("hello world!")
+        Console.WriteLine("from Anthony!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Int(v As Integer)
+
+    End Sub
+
+    Private Sub Int()
+        Throw New NotImplementedException()
+    End Sub
+End Module
