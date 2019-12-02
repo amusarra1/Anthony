02cd69cd4ac7a6e83841e76720b697438cba1a98
amusarra1 <56942183+amusarra1@users.noreply.github.com>
2019-12-02 16:13:13 -0500

    Add files via upload

---
+++ Lab 1 - Anthony Musarra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab_1__Anthony_Musarra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Anthony!");
+
+            Console.WriteLine("Hello world");
+            Console.WriteLine("From Anthony");
+        }
+    }
+}
