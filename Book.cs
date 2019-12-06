b3370948663116a15afd8482c65c16d7284352c1
amusarra1 <56942183+amusarra1@users.noreply.github.com>
2019-12-06 13:09:55 -0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_7___Anthony_Musarra
+{
+       class Book
+    {
+
+        private string Title;
+        private string Author;
+        private int Year;
+       
+        public Book(string Title, string Author, int Year)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Year = Year;
+        }
+        public Book(string Title, int Rating, string Director)
+        {
+            this.Title = Title;
+            this.Author = Director;
+          
+        }
+
+        public Book(string Title)
+        {
+            this.Title = Title;
+        }
+        public void Display()
+        {
+            Console.WriteLine("{0} directed by {1} released in {2}\n", Title, Author, Year);
+        }
+
+
+        // Question 3: 
+
+
+
+
+
+
+    }
+
+        
+        
+       
+
+
+
+
+    }
---
+++ BookTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_7___Anthony_Musarra
+{
+    class BookTest
+    {
+       static void Main(string[] args)
+        {
+           Book myBook = new Book("Green Eggs and Ham", "Dr. Suess", 1960);
+
+             myBook.Display();
+           
+
+        }
+    }
+}
